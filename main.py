03d7ede3a22a4c42c8afdeefe60ad33310a86cd8
ABHISHEK T K <109537560+789Abhi@users.noreply.github.com>
2023-02-18 06:20:53 +0000

    updated with new class

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 class Dog:
 
-	def __init__(self, i, j):
+	def __init__(self, i, j, k):
 		self.i = i
 		self.j = j
+		self.k = k
 
 	def name(self):
 		print("charlii")
@@ -10,9 +11,9 @@ class Dog:
 	def desc(self):
 		print("Age is", self.i)
 		print("Live in", self.j)
+		print("Looks after by Alape family", self.k)
 
 
-my_dog = Dog("1 years", "Mangalore")
+my_dog = Dog("1 years", "Mangalore", "Abhishek")
 my_dog.name()
 my_dog.desc()
-   gfdsdfg
