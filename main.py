6bc936c86409b3ec6d5654445a641dd3898c0c65
ABHISHEK <>
2023-02-18 06:06:09 +0000

    complete change

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 class Dog:
-	def __init__(self,i,j):
-		self.i=i
-		self.j=j
+
+	def __init__(self, i, j):
+		self.i = i
+		self.j = j
+
 	def name(self):
 		print("charlii")
-	def desc(self):
-		print("Age is",self.i)
-		print("Live in",self.j)
 
-my_dog=Dog("1 years","Mangalore")
+	def desc(self):
+		print("Age is", self.i)
+		print("Live in", self.j)
+
+
+my_dog = Dog("1 years", "Mangalore")
 my_dog.name()
 my_dog.desc()
-	
