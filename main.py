e28f6e2efc3d03d4c5d3639fe636c477d3c05fa5
ABHISHEK T K <109537560+789Abhi@users.noreply.github.com>
2023-02-28 15:27:56 +0000

    Strings in python

--- main.py
+++ main.py
@@ -1,170 +1,527 @@
-# # # # # # # # # # # # with open("abhi.txt",'w') as files:
-# # # # # # # # # # # # 	files.write("hello abhi")
+# # # # # # # # # # # # # # # # # # # # # with open("abhi.txt",'w') as files:
+# # # # # # # # # # # # # # # # # # # # # 	files.write("hello abhi")
 
-# # # # # # # # # # # import math
-# # # # # # # # # # # print(math.factorial(5))
+# # # # # # # # # # # # # # # # # # # # import math
+# # # # # # # # # # # # # # # # # # # # print(math.factorial(5))
 
-# # # # # # # # # # n=10
-# # # # # # # # # # for i in range(n):
-# # # # # # # # # #  print(i,end=" ")
+# # # # # # # # # # # # # # # # # # # n=10
+# # # # # # # # # # # # # # # # # # # for i in range(n):
+# # # # # # # # # # # # # # # # # # #  print(i,end=" ")
 
-# # # # # # # # # g=lambda x:x*x
-# # # # # # # # # print(g(2))
-# # # # # # # # from math import factorial
-# # # # # # # # print(factorial(5))
+# # # # # # # # # # # # # # # # # # g=lambda x:x*x
+# # # # # # # # # # # # # # # # # # print(g(2))
+# # # # # # # # # # # # # # # # # from math import factorial
+# # # # # # # # # # # # # # # # # print(factorial(5))
 
-# # # # # # # i="Abhishek"
-# # # # # # # if "f" in i:
-# # # # # # # 	print("true")
-# # # # # # # else:
-# # # # # # # 	print("false")
+# # # # # # # # # # # # # # # # i="Abhishek"
+# # # # # # # # # # # # # # # # if "f" in i:
+# # # # # # # # # # # # # # # # 	print("true")
+# # # # # # # # # # # # # # # # else:
+# # # # # # # # # # # # # # # # 	print("false")
 
-# # # # # a=4
-# # # # # b=4
-# # # # # try:
-# # # # # 	k=a//b
-# # # # # 	print(int(k))
+# # # # # # # # # # # # # # a=4
+# # # # # # # # # # # # # # b=4
+# # # # # # # # # # # # # # try:
+# # # # # # # # # # # # # # 	k=a//b
+# # # # # # # # # # # # # # 	print(int(k))
 
-# # # # # except ZeroDivisionError :
-# # # # # 	print("cant divide by zero ")
-# # # # # finally:
-# # # # # 	print("problem solved")
+# # # # # # # # # # # # # # except ZeroDivisionError :
+# # # # # # # # # # # # # # 	print("cant divide by zero ")
+# # # # # # # # # # # # # # finally:
+# # # # # # # # # # # # # # 	print("problem solved")
 
-# # # # # print ("The value of a / b is : ")
-# # # # # assert b != 0, "Divide by 0 error"
-# # # # # print (a / b)
+# # # # # # # # # # # # # # print ("The value of a / b is : ")
+# # # # # # # # # # # # # # assert b != 0, "Divide by 0 error"
+# # # # # # # # # # # # # # print (a / b)
 
-# # # # a=22
-# # # # b='Abhishek'
-# # # # print(a)
-# # # # print(b)
-# # # # del a
-# # # # del b
-# # # # a="nachi"
-# # # # b="18"
-# # # # print(a)
-# # # # print(b)
+# # # # # # # # # # # # # a=22
+# # # # # # # # # # # # # b='Abhishek'
+# # # # # # # # # # # # # print(a)
+# # # # # # # # # # # # # print(b)
+# # # # # # # # # # # # # del a
+# # # # # # # # # # # # # del b
+# # # # # # # # # # # # # a="nachi"
+# # # # # # # # # # # # # b="18"
+# # # # # # # # # # # # # print(a)
+# # # # # # # # # # # # # print(b)
 
-# # # a=10
-# # # b=20
-# # # def add():
-# # # 	c=a+b
-# # # 	print(c)
-# # # add()
+# # # # # # # # # # # # a=10
+# # # # # # # # # # # # b=20
+# # # # # # # # # # # # def add():
+# # # # # # # # # # # # 	c=a+b
+# # # # # # # # # # # # 	print(c)
+# # # # # # # # # # # # add()
 
-# # # def fun():
-# # # 	var=150
-# # # 	def gun():
-# # # 		nonlocal var
-# # # 		var=var+50
-# # # 		print(var)
-# # # 	gun()
-# # # fun()
+# # # # # # # # # # # # def fun():
+# # # # # # # # # # # # 	var=150
+# # # # # # # # # # # # 	def gun():
+# # # # # # # # # # # # 		nonlocal var
+# # # # # # # # # # # # 		var=var+50
+# # # # # # # # # # # # 		print(var)
+# # # # # # # # # # # # 	gun()
+# # # # # # # # # # # # fun()
 
-# # # a,b,c=1,3,5
-# # # print(a+b+c)
-# # # g = input("Enter your name : ")
-# # # print(g)
+# # # # # # # # # # # # a,b,c=1,3,5
+# # # # # # # # # # # # print(a+b+c)
+# # # # # # # # # # # # g = input("Enter your name : ")
+# # # # # # # # # # # # print(g)
 
-# # # a, b, c, d = input("enter 2 value:").split()
-# # # print(a)
-# # # print(b)
-# # # print(c)
-# # # print(d)
+# # # # # # # # # # # # a, b, c, d = input("enter 2 value:").split()
+# # # # # # # # # # # # print(a)
+# # # # # # # # # # # # print(b)
+# # # # # # # # # # # # print(c)
+# # # # # # # # # # # # print(d)
 
-# # # taking multiple inputs at a time 
-# # # and type casting using list() function
-# # # x = list(input("Enter multiple values: ").split())
-# # # print("List of students: ",x)
-# # # x = set(input("Enter multiple values: ").split())
-# # # print("List of students: ",x)
+# # # # # # # # # # # # taking multiple inputs at a time
+# # # # # # # # # # # # and type casting using list() function
+# # # # # # # # # # # # x = list(input("Enter multiple values: ").split())
+# # # # # # # # # # # # print("List of students: ",x)
+# # # # # # # # # # # # x = set(input("Enter multiple values: ").split())
+# # # # # # # # # # # # print("List of students: ",x)
+
+# # # # # # # # # # # # x,y=[int(x) for x in input("enter 2 values:").split()]
+# # # # # # # # # # # # print(x,y)
+
+# # # # # # # # # # # # x=10
+# # # # # # # # # # # # y=20
+# # # # # # # # # # # # print("value of x is {} and value of y is {}".format(y,x))
+
+# # # # # # # # # # # # x=set(int(x) for x in input("ENter multiple value:").split(','))
+# # # # # # # # # # # # print(x)
+# # # # # # # # # # # # import time
+# # # # # # # # # # # # count_down=5
+# # # # # # # # # # # # for i in reversed(range(count_down+1)):
+
+# # # # # # # # # # # # 	if i>0:
+# # # # # # # # # # # # 		print(i,end=">>>",flush='true')
+# # # # # # # # # # # # 		time.sleep(1)
+# # # # # # # # # # # # 	else:
+# # # # # # # # # # # # 		print("start")
+
+# # # # # # # # # # # # x=20
+# # # # # # # # # # # # y=2
+# # # # # # # # # # # # z=2023
+# # # # # # # # # # # # print(x,y,z,sep='-')
+# # # # # # # # # # # # import io
+
+# # # # # # # # # # # # # declare a dummy file
+# # # # # # # # # # # # dummy_file = io.StringIO()
+
+# # # # # # # # # # # # # add message to the dummy file
+# # # # # # # # # # # # print('Hello Geeks!!', file=dummy_file)
+
+# # # # # # # # # # # # # get the value from dummy file
+# # # # # # # # # # # # dummy_file.getvalue()
+
+# # # # # # # # # # # # import io
+# # # # # # # # # # # # dummy_file = io.StringIO()
+# # # # # # # # # # # # print('Abhishek ', file=dummy_file)
+# # # # # # # # # # # # dummy_file.getvalue()
+
+# # # # # # # # # # # print("Hello Abhishek You are learnig Python",file=open('abi.html','w'))
+
+# # # # # # # # # # a=[1,2,3,4]
+# # # # # # # # # # for i in range(4):
+# # # # # # # # # # 	print(a[i],end=" ")
+
+# # # # # # # # # # a=[1,3,5,7,9]
+# # # # # # # # # # b=[2,4,5,6,8,8]
+# # # # # # # # # # print(set( a + b))
+# # # # # # # # # #import the below module and see what happens
+# # # # # # # # # # import antigravity
+# # # # # # # # # #NOTE - it wont work on online ide
+
+# # # # # # # # # # abhi="Abhishek"
+# # # # # # # # # # print(abhi.center(20,'-'))
+
+# # # # # # # # # # a=True
+# # # # # # # # # # b=False
+# # # # # # # # # # print(a and b)
+
+# # # # # # # # # # a=10
+# # # # # # # # # # b=4
+# # # # # # # # # # print(a & b)
+# # # # # # # # # # print(a | b)
+# # # # # # # # # # print(~a)
+# # # # # # # # # # print(a^b)
+# # # # # # # # # # print(a>>2)
+# # # # # # # # # # print(a<<2)
+
+# # # # # # # # # # x=24
+# # # # # # # # # # y=20
+# # # # # # # # # # list=[10,20,30,40,50]
+# # # # # # # # # # if(x not in list):
+# # # # # # # # # # 	print("X not present")
+# # # # # # # # # # else:
+# # # # # # # # # # 	print("x is present")
+
+# # # # # # # # # # a=1+20*10
+# # # # # # # # # # print(a)
+
+# # # # # # # # # # a=140
+# # # # # # # # # # b=20
+# # # # # # # # # # min=a if a<b else b
+# # # # # # # # # # print(min)
+
+# # # # # # # # # # a = 10
+# # # # # # # # # # b = 20
+# # # # # # # # # # # print((b,a) [a>b])
+# # # # # # # # # # print({True: a, False: b}[a < b])
+# # # # # # # # # # Python Program illustrate how
+# # # # # # # # # # to overload an binary + operator
+# # # # # # # # # # And how it actually works
+
+# # # # # # # # # # class Ab:
+
+# # # # # # # # # # 	def __init__(self, a):
+# # # # # # # # # # 		self.a = a
+
+# # # # # # # # # 	# adding two objects
+# # # # # # # # # # 	def __add__(self, o):
+# # # # # # # # # # 		return self.a + o.a
+
+# # # # # # # # # # ob1 = Ab("1")
+# # # # # # # # # # ob2 = Ab("2")
+# # # # # # # # # # ob3 = Ab("Geeks")
+# # # # # # # # # # ob4 = Ab("For")
+
+# # # # # # # # # # print(ob1 + ob2)
+# # # # # # # # # # print(ob3 + ob4)
+# # # # # # # # # # print(Ab.__add__(ob1, ob4))
+# # # # # # # # # # print(ob1.__add__(ob2))
+# # # # # # # # # # Actual working when Binary Operator is used.
+# # # # # # # # # # print(A.__add__(ob1 , ob2))
+# # # # # # # # # # print(A.__add__(ob3,ob4))
+# # # # # # # # # # #And can also be Understand as :
+# # # # # # # # # # print(ob1.__add__(ob2))
+# # # # # # # # # # print(ob3.__add__(ob4))
+
+# # # # # # # # # # class Ab:
+# # # # # # # # # # 	def __init__(self,a,b):
+# # # # # # # # # # 		self.a=a
+# # # # # # # # # # 		self.b=b
+# # # # # # # # # # 	def __add__(self,other):
+# # # # # # # # # # 		return self.a + other.a, self.b + other.b
+
+# # # # # # # # # # ob1=Ab(1,2)
+# # # # # # # # # # ob2=Ab(2,7)
+
+# # # # # # # # # # print(Ab.__add__(ob1,ob2))
+
+# # # # # # # # # # Python program to overload
+# # # # # # # # # # a comparison operators
+
+# # # # # # # # # # class A:
+# # # # # # # # # # 	def __init__(self,a):
+# # # # # # # # # # 		self.a=a
+# # # # # # # # # # 		def __chk__(self,other):
+# # # # # # # # # # 			if(self.a > other.a):
+# # # # # # # # # # 				return True
+# # # # # # # # # # 			else:
+# # # # # # # # # # 				return
+
+# # # # # # # # # # ob1=A(2)
+# # # # # # # # # # ob2=A(3)
+# # # # # # # # # # if (ob1>ob2):
+# # # # # # # # # # 	print("Ob1 is greater")
+# # # # # # # # # # else:
+# # # # # # # # # # 	print("Ob2 is greater")
+
+# # # # # # # # # # a=[1,2,3,4,5,6]
+# # # # # # # # # # b=reversed(a)
+# # # # # # # # # # print(b)
+
+# # # # # # # # # # a=(1,2,3,4,5,6,7,8,9)
+# # # # # # # # # # rev=a[::-1]
+# # # # # # # # # # print(rev)
+
+# # # # # # # # # # class A:
+# # # # # # # # # # 	def __init__(self,a):
+# # # # # # # # # # 		self.a=a
+
+# # # # # # # # # # 	def __invert__(self):
+# # # # # # # # # # 		return "This is Operator"
+
+# # # # # # # # # # ob1=A(2)
+
+# # # # # # # # # # print(~ob1)
+
+# # # # # # # # # # print(any([True,True,True]))
+# # # # # # # # # # print(any([True,False,True]))
+# # # # # # # # # # print(any([False,False,True]))
+# # # # # # # # # # print("\n")
+# # # # # # # # # # print(all([True,True,True]))
+# # # # # # # # # # print(all([True,False,True]))
+# # # # # # # # # # print(all([False,False,True]))
+
+# # # # # # # # # # list1=[]
+# # # # # # # # # # list2=[]
+# # # # # # # # # # for i in range(1,11):
+# # # # # # # # # # 	list1.append(4*i)
+# # # # # # # # # # 	list2.append(5*i)
+
+# # # # # # # # # # print(list1)
+# # # # # # # # # # print(list2)
+
+# # # # # # # # # # list1=[]
+# # # # # # # # # # list2=[]
+
+# # # # # # # # # # for i in range(1,11):
+# # # # # # # # # # 	list1.append(3*i)
+
+# # # # # # # # # # for i in range(0,10):
+# # # # # # # # # # 	list2.append(list1[i]%5==0)
+
+# # # # # # # # # # print(list1)
+# # # # # # # # # # print(all(list2))
+
+# # # # # # # # # # import operator
+# # # # # # # # # # a=10
+# # # # # # # # # # b=10
+# # # # # # # # # # print(operator.ne(a,b))
+
+# # # # # # # # # # import operator
+# # # # # # # # # # li=[1,2,4,7,9,3,10]
+
+# # # # # # # # # # for i in range(0,len(li)):
+# # # # # # # # # # 	print(li[i],end=",")
+
+# # # # # # # # # # operator.delitem(li,3)
+# # # # # # # # # # print('\n')
+# # # # # # # # # # for i in range (0,len(li)):
+# # # # # # # # # # 		print(li[i],end=",")
+
+# # # # # # # # # # import operator
+# # # # # # # # # # list=[1,2,3,4,9,8,7,6,5]
+
+# # # # # # # # # # for i in range(0,len(list)):
+# # # # # # # # # # 	print(list[i],end=",")
+# # # # # # # # # # print('\n')
+# # # # # # # # # # print('4th item in the list is :',end=' ')
+# # # # # # # # # # print(operator.setitem(list,4,98))
+
+# # # # # # # # # # for i in range(0,len(list)):
+# # # # # # # # # # 	print(list[i],end=",")
+# # # # # # # # # # print('\n')
+# # # # # # # # # # operator.delitem(list,1)
+
+# # # # # # # # # # for i in range(0,len(list)):
+# # # # # # # # # # 	print(list[i],end=",")
+
+# # # # # # # # # # import operator
+# # # # # # # # # # a="abhi"
+# # # # # # # # # # b="shek"
+# # # # # # # # # # c=operator.concat(a,b)
+# # # # # # # # # # d=c.upper()
+# # # # # # # # # # print(d)
+
+# # # # # # # # # # a=10
+# # # # # # # # # # print(a+a)
+# # # # # # # # # # a=20
+# # # # # # # # # # print(a)
+
+# # # # # # # # # # a='''Hifd'''
+# # # # # # # # # # print(a)
+
+# # # # # # # # # # import operator
+# # # # # # # # # # L = [1, "a" , "string" , 1+2]
+# # # # # # # # # # print(L)
+# # # # # # # # # # operator.setitem(L,3,10)
+# # # # # # # # # # print(L)
+# # # # # # # # # # L.pop()
+# # # # # # # # # # print(L)
+# # # # # # # # # # L.pop()
+# # # # # # # # # # print(L)
+# # # # # # # # # # L.pop()
+# # # # # # # # # # print(L)
+
+# # # # # # # # # # list=(1,2,3,"abhi",1+10)
+# # # # # # # # # # for i in range(0,len(list)-2):
+# # # # # # # # # # 	print(list[i],end=" ")
+
+# # # # # # # # # # i=0
+# # # # # # # # # # while(i<=10):
+# # # # # # # # # # 	print((i*2)+1,end=" ")
+# # # # # # # # # # 	i+=1
+# # # # # # # # # # a="abhi"
+# # # # # # # # # # print(a[-2])
+
+# # # # # # # # # # def reversed(s):
+# # # # # # # # # # 	str=""
+# # # # # # # # # # 	for i in s:
+# # # # # # # # # # 		str=i+str
+# # # # # # # # # # 	return str
+
+# # # # # # # # # # s="abhi"
+# # # # # # # # # # print("Original string is" ,s,"\n",end="")
+# # # # # # # # # # print("Reversed string is ",end="")
+# # # # # # # # # # print(reversed(s))
+
+# # # # # # # # # # def rev(s):
+# # # # # # # # # # 	if len(s)==0:
+# # # # # # # # # # 		return s
+# # # # # # # # # # 	else:
+# # # # # # # # # # 		return rev(s[1:])+s[0]
+
+# # # # # # # # # # s="Abhishek"
+# # # # # # # # # # print("Original string is",end=" ")
+# # # # # # # # # # print(s)
+# # # # # # # # # # print("Reversed String is",end=" ")
+# # # # # # # # # # print(rev(s))
+
+# # # # # # # # # def createStack():
+# # # # # # # # # 	stack=[]
+# # # # # # # # # 	return stack
+
+# # # # # # # # # def push(stack,item):
+# # # # # # # # # 	stack.append(item)
+
+# # # # # # # # # def size(stack):
+# # # # # # # # # 	return len(stack)
+
+# # # # # # # # # def isempty(stack):
+# # # # # # # # # 	if size(stack)==0:
+# # # # # # # # # 		return True
+
+# # # # # # # # # def pop(stack):
+# # # # # # # # # 	if isempty(stack):
+# # # # # # # # # 		return
+# # # # # # # # # 	else:
+# # # # # # # # # 		return stack.pop()
+
+# # # # # # # # # def rev(string):
+# # # # # # # # # 	n=len(string)
+
+# # # # # # # # #   # creating Stack
+# # # # # # # # # 	stack=createStack()
+
+# # # # # # # # # 	# pushing string to stack
+# # # # # # # # # 	for i in range(0,n,1):
+# # # # # # # # # 		push(stack,string[i])
+
+# # # # # # # # # 	string=""
+
+# # # # # # # # # 	for i in range(0,n,1):
+# # # # # # # # # 		string=string+pop(stack)
+# # # # # # # # # 	return string
+
+# # # # # # # # # s="Abhishek"
+# # # # # # # # # print("Original string is",end=" ")
+# # # # # # # # # print(s)
+# # # # # # # # # print("Reversed String is",end=" ")
+# # # # # # # # # print(rev(s))
 
 
-# # # x,y=[int(x) for x in input("enter 2 values:").split()]
-# # # print(x,y)
+# # # # # # # # def createstack():
+# # # # # # # # 	stack = []
+# # # # # # # # 	return stack
 
-# # # x=10
-# # # y=20
-# # # print("value of x is {} and value of y is {}".format(y,x))
 
-# # # x=set(int(x) for x in input("ENter multiple value:").split(','))
-# # # print(x)
-# # # import time
-# # # count_down=5
-# # # for i in reversed(range(count_down+1)):
-	
-# # # 	if i>0:
-# # # 		print(i,end=">>>",flush='true')
-# # # 		time.sleep(1)
-# # # 	else:
-# # # 		print("start")
+# # # # # # # # def push(stack, item):
+# # # # # # # # 	stack.append(item)
 
-# # # x=20
-# # # y=2
-# # # z=2023
-# # # print(x,y,z,sep='-')
-# # # import io
 
-# # # # declare a dummy file
-# # # dummy_file = io.StringIO()
+# # # # # # # # def size(stack):
+# # # # # # # # 	return len(stack)
 
-# # # # add message to the dummy file
-# # # print('Hello Geeks!!', file=dummy_file)
 
-# # # # get the value from dummy file
-# # # dummy_file.getvalue()
+# # # # # # # # def isempty(stack):
+# # # # # # # # 	if size(s) == 0:
+# # # # # # # # 		return True
 
-# # # import io
-# # # dummy_file = io.StringIO()
-# # # print('Abhishek ', file=dummy_file)
-# # # dummy_file.getvalue()
 
-# # print("Hello Abhishek You are learnig Python",file=open('abi.html','w'))
+# # # # # # # # def pop(stack):
+# # # # # # # # 	if isempty(stack):
+# # # # # # # # 		return
+# # # # # # # # 	else:
+# # # # # # # # 		return stack.pop()
 
-# a=[1,2,3,4]
-# for i in range(4):
-# 	print(a[i],end=" ")
 
-# a=[1,3,5,7,9]
-# b=[2,4,5,6,8,8]
-# print(set( a + b))
-#import the below module and see what happens
-# import antigravity
-#NOTE - it wont work on online ide
+# # # # # # # # def rev(s):
+# # # # # # # # 	n = len(s)
+# # # # # # # # 	stack = createstack()
 
-# abhi="Abhishek"
-# print(abhi.center(20,'-'))
+# # # # # # # # 	for i in range(0, n, 1):
+# # # # # # # # 		push(stack, s[i])
 
-# a=True
-# b=False
-# print(a and b)  
+# # # # # # # # 	string = ""
 
-# a=10
-# b=4
-# print(a & b)
-# print(a | b)
-# print(~a)
-# print(a^b)
-# print(a>>2)
-# print(a<<2)
+# # # # # # # # 	for i in range(0, n, 1):
+# # # # # # # # 		string = string + pop(stack)
+# # # # # # # # 	return string
 
-# x=24
-# y=20
-# list=[10,20,30,40,50]
-# if(x not in list):
-# 	print("X not present")
-# else:
-# 	print("x is present")
 
-# a=1+20*10
-# print(a)
+# # # # # # # # s = "Abhishek"
+# # # # # # # # print("Orginal string is", end="")
+# # # # # # # # print(s)
+# # # # # # # # print("Reversed String is ", end="")
+# # # # # # # # print(rev(s))
 
-# a=140
-# b=20
-# min=a if a<b else b
-# print(min)
 
-a=10
-b=20
-# print((b,a) [a>b])
-print({True: a, False: b} [a < b])
+# # # # # # # a="abhishek"
+# # # # # # # a="".join(reversed(a))
+# # # # # # # print(a)
+
+# # # # # # # a="abhishektk"
+# # # # # # # print(a[3:-1])
+
+# # # # # # a="hello this is string"
+# # # # # # lst=list(a)
+# # # # # # print(lst)
+
+# # # # # # lst[9]='p'
+# # # # # # lst2=''.join(lst)
+# # # # # # print(lst2)
+
+# # # # # # a='abhiishek'
+# # # # # # b=a[0:4]+a[5:]
+# # # # # # print(b)
+
+# # # # # # a='abhi'
+# # # # # # del a
+# # # # # # print(a)
+
+# # # # # # a="abhishek is a good"
+# # # # # # b=""
+# # # # # # print(b.endswith(a))
+# # # # # # a="Abhishek is a good boy"
+# # # # # # print(a.upper())
+# # # # # # print(a.lower())
+# # # # # # print(a.swapcase())
+# # # # # # print(a.title())
+
+# # # # # # a="abhishek is a good boy"
+# # # # # # if 'isa' in a:
+# # # # # # 	print("string exists")
+# # # # # # else:
+# # # # # # 	print("Not exists")
+# # # # # def check(a,b):
+# # # # # 	if(a.find(b)==-1):
+# # # # # 		print("string not exists")
+# # # # # 	else:
+# # # # # 		print("exists")
+# # # # # a="abhishek is a good boy"
+# # # # # b="is ab"
+
+# # # # # check(a,b)
+
+# # # # import re
+# # # # a="abhishek has lenovo laptop"
+# # # # b="ek h"
+# # # # if re.search(b,a):
+# # # # 	print("present")
+# # # # else:
+# # # # 	print("Not present")
+# # # a="abhishek has lenovo laptop"
+# # # b="has"
+# # # x=list(filter(lambda x: (b in a),a.split()))
+# # # print(["yes" if x else "no"])
+
+# # import operator as op
+# # a="abhishek is "
+# # b="is"
+# # print(["yes" if op.countOf(a.split(),b)>0 else "no"])
+
+# a="gbeezksforgeeks"
+# print(min(a))
+# print(max(a))
